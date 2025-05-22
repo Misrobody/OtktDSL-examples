f6c32103c5c8abc5a2cf60e262261d1700d77df8
larrivain <daphne.larrivain@ecole.ensicaen.fr>
2025-05-22 14:44:02 +0200

    buildable helloWorld

---
+++ apps/helloWorld/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="helloWorld",
+    version="1.0.0",
+    description="Dummy python module for instrumentation purposes",
+    url="https://github.com/kieker-monitoring/OtktDSL",
+)
